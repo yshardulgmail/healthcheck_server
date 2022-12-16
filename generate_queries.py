f925a93f7b7cd0c0647709a2594c42e18c97956f
Yogesh Shardul <yogesh@Yogeshs-MacBook-Pro.local>
2022-12-16 17:47:22 +0530

    mauuus helper

--- generate_queries.py
+++ generate_queries.py
@@ -4,12 +4,15 @@ dataframe2 = pd.read_excel("Book1.xlsx")
 servers = list(dataframe2.columns.values)[1:]
 insert_query = "insert into applications values(#id#, '#app_name#', '#app_url#', '#server#');"
 
-print(servers)
+queries = []
 id = 1
 for server in servers:
     for values in dataframe2[["Application", server]].values:
-        print(insert_query.replace("#app_name#", values[0])
+        queries.append(insert_query.replace("#app_name#", values[0])
                           .replace("#app_url#", values[1])
                           .replace("#server#", server)
                           .replace("#id#", str(id)))
-        id = id + 1    
+        id = id + 1   
+
+with open("insert_queries.txt", "w") as fp:
+    fp.write("\n".join(queries))
