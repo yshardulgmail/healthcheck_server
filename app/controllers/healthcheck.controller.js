89811c9e4818932cbeb4d99266c902e1343aaebb
Yogesh Shardul <yogesh@Yogeshs-MacBook-Pro.local>
2022-12-20 19:41:13 +0530

    working with refresh

--- app/controllers/healthcheck.controller.js
+++ app/controllers/healthcheck.controller.js
@@ -27,52 +27,52 @@ exports.findAllAppStatus = (req, res) => {
     if (err) console.log(err);
 
     var request = new sql.Request();
-       
+
     request.query("select app.app_id APP_ID, app_name, app_url, server, format(check_time, 'hh:mm tt') as check_time, status from applications app inner join app_status s on app.app_id = s.app_id order by check_time", function (err, recordset) {
-        
-        if (err) console.log(err)
-        const resultSet = [...recordset.recordsets[0]];
-        
-        if(manual == "true") {
-          let current_time = "";
-          request.query("select format(GETDATE(), 'hh:mm tt') as check_time", function (err, timeStr) {
-            current_time = timeStr["recordset"][0]["check_time"];
-            console.log(current_time);
+
+      if (err) console.log(err)
+      const resultSet = [...recordset.recordsets[0]];
+
+      if (manual == "true") {
+        let current_time = "";
+        request.query("select format(GETDATE(), 'hh:mm tt') as check_time", function (err, timeStr) {
+          current_time = timeStr["recordset"][0]["check_time"];
+          console.log(current_time);
+        });
+
+        Applications.findAll({
+          raw: true,
+        })
+          .then((data) => {
+            Promise.all(data.map(appData => {
+              appUrl = appData["app_url"];
+              return fetch(appUrl).then(res1 => res1.text())
+                .then(text => {
+                  let status = "DOWN";
+
+                  // This is random status generator. Need to remove when app urls are working
+                  // const statuses = ["UP", "DOWN"];
+                  // let status = statuses[Math.floor(Math.random() * statuses.length)];
+                  if (text.includes("UP") || text.toLowerCase().includes("running")) {
+                    status = "UP";
+                  }
+                  const newAppData = {};
+                  newAppData["APP_ID"] = appData["app_id"];
+                  newAppData["app_name"] = appData["app_name"];
+                  newAppData["app_url"] = appData["app_url"];
+                  newAppData["server"] = appData["server"];
+                  newAppData["check_time"] = current_time;
+                  newAppData["status"] = status;
+                  return newAppData;
+                });
+            })).then(data1 => {
+              data1.map(newAppData => resultSet.push(newAppData));
+              console.log(resultSet.length);
+              res.send(resultSet);
+            });
           });
-          
-          Applications.findAll({
-            raw: true,
-            })
-            .then((data) => {
-              Promise.all(data.map(appData => {
-                  appUrl = appData["app_url"];
-                  return fetch(appUrl).then(res1 => res1.text())
-                  .then(text => {
-                    let status = "DOWN";
-    
-                    // This is random status generator. Need to remove when app urls are working
-                    // const statuses = ["UP", "DOWN"];
-                    // let status = statuses[Math.floor(Math.random() * statuses.length)];
-                    if(text.includes("UP") || text.toLowerCase().includes("running")) {
-                      status = "UP";
-                    }
-                    const newAppData = {};
-                    newAppData["APP_ID"] = appData["app_id"];
-                    newAppData["app_name"] = appData["app_name"];
-                    newAppData["app_url"] = appData["app_url"];
-                    newAppData["server"] = appData["server"];
-                    newAppData["check_time"] = current_time;
-                    newAppData["status"] = status;
-                    return newAppData;
-                  });
-                })).then(data1 => {
-                  data1.map(newAppData => resultSet.push(newAppData));
-                  console.log(resultSet.length);
-                  res.send(resultSet);
-                });   
-              });
-        
-      }  
+
+      }
       else {
         res.send(resultSet);
       }
@@ -81,14 +81,14 @@ exports.findAllAppStatus = (req, res) => {
 };
 
 exports.findAllApplications = (req, res) => {
-  Applications.hasMany(AppStatus, {foreignKey: 'app_id'})
-  AppStatus.belongsTo(Applications, {foreignKey: 'app_id'});
+  Applications.hasMany(AppStatus, { foreignKey: 'app_id' })
+  AppStatus.belongsTo(Applications, { foreignKey: 'app_id' });
   Applications.findAll({
     include: [{
       model: AppStatus,
       // where: {app_id: AppStatus.app_id},
       required: true
-  }]
+    }]
   })
     .then(data => {
       res.send(data);
@@ -117,29 +117,29 @@ exports.findApplications = (req, res) => {
 exports.findApplicationStatus = (req, res) => {
   console.log(req.params.appId);
   Applications.findAll({
-    where: {app_id: req.params.appId},
+    where: { app_id: req.params.appId },
   })
     .then(data => {
-      if(data.length > 0) {
+      if (data.length > 0) {
         appUrl = data[0]["dataValues"]["app_url"]
         fetch(appUrl)
-        .then(res => res.text())
-        .then(text => {
-          console.log("fetched", text)
-          // let status = "DOWN";
+          .then(res => res.text())
+          .then(text => {
+            console.log("fetched", text)
+            // let status = "DOWN";
 
-          // This is random status generator. Need to remove when app urls are working
-          const statuses = ["UP", "DOWN"];
-          let status = statuses[Math.floor(Math.random() * statuses.length)];
+            // This is random status generator. Need to remove when app urls are working
+            const statuses = ["UP", "DOWN"];
+            let status = statuses[Math.floor(Math.random() * statuses.length)];
 
-          if(text.includes("UP") || text.toLowerCase().includes("running")) {
-            status = "UP";
-          }
+            if (text.includes("UP") || text.toLowerCase().includes("running")) {
+              status = "UP";
+            }
 
-          res.send('"status": "' + status + '"')
-        });
+            res.send('"status": "' + status + '"')
+          });
       }
-      else{
+      else {
         throw Error("Application not found");
       }
     })
@@ -167,44 +167,44 @@ exports.findAppStatus = (req, res) => {
 exports.saveAppStatus = () => {
   Applications.findAll()
     .then(data => {
-      
+
       for (const [key, value] of Object.entries(data)) {
         console.log(value["dataValues"]);
         appUrl = value["dataValues"]["app_url"]
         fetch(appUrl)
-        .then(res => res.text())
-        .then(text => {
-          console.log("fetched")
-          // let status = "DOWN";
+          .then(res => res.text())
+          .then(text => {
+            console.log("fetched")
+            // let status = "DOWN";
 
-          // This is random status generator. Need to remove when app urls are working
-          const statuses = ["UP", "DOWN"];
-          let status = statuses[Math.floor(Math.random() * statuses.length)];
+            // This is random status generator. Need to remove when app urls are working
+            const statuses = ["UP", "DOWN"];
+            let status = statuses[Math.floor(Math.random() * statuses.length)];
 
-          const nowTime =  new Date();
-          if(text.includes("UP") || text.toLowerCase().includes("running")) {
-            status = "UP";
-          }
+            const nowTime = new Date();
+            if (text.includes("UP") || text.toLowerCase().includes("running")) {
+              status = "UP";
+            }
 
-          sql.connect(sqlConfig, function (err) {
-            if (err) console.log(err);        
-            const request = new sql.Request();
-            const date = new Date();
-            let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-            const am_pm = date.getHours() >= 12 ? "PM" : "AM";
-            hours = hours < 10 ? "0" + hours : hours;
-            const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-            const time = hours + ":" + minutes + am_pm;
-            const insertQuery = "insert into app_status values(" + value["dataValues"]["app_id"] 
-                                                                 + ", CAST(format(GETDATE(), 'hh:mm') as TIME), '" 
-                                                                 + status + "')"
-            request.query(insertQuery, function (err, recordset) {
+            sql.connect(sqlConfig, function (err) {
+              if (err) console.log(err);
+              const request = new sql.Request();
+              const date = new Date();
+              let hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+              const am_pm = date.getHours() >= 12 ? "PM" : "AM";
+              hours = hours < 10 ? "0" + hours : hours;
+              const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+              const time = hours + ":" + minutes + am_pm;
+              const insertQuery = "insert into app_status values(" + value["dataValues"]["app_id"]
+                + ", GETDATE(), '"
+                + status + "')"
+              request.query(insertQuery, function (err, recordset) {
                 if (err) console.log(err)
                 // res.send(recordset.recordsets[0]);
+              });
+              console.log(insertQuery);
             });
-            console.log(insertQuery);
           });
-        });
       }
     })
     .catch(err => {
